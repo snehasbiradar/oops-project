7cd95de5e1156d09dcba3b3c4b9eb91f886bfdae
snehasbiradar <149710151+snehasbiradar@users.noreply.github.com>
2024-02-02 00:49:03 +0530

    Add files via upload

---
+++ Skin-Care-Manager/Coursework/src/Patient.java
@@ -0,0 +1,56 @@
+public class Patient extends Person{
+
+    private String title;
+    private String id;
+    private String mail;
+    private String city;
+
+
+    public Patient(String name, String surname, String dob, String mobile_number, String title, String id, String mail, String city){
+        super(name, surname, dob, mobile_number);
+        this.title = title;
+        this.id= id;
+        this.mail = mail;
+        this.city = city;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public void setMail(String mail){
+        this.mail = mail;
+    }
+
+    public String getMail(){
+        return mail;
+    }
+
+    public void setCity(String city){
+        this.city = city;
+    }
+
+    public String getCity(){
+        return city;
+    }
+
+    public String toString(){
+        return super.toString() +
+                "\nTitle = " + title +
+                "\nID = "  + id +
+                "\nMail = "+ mail +
+                "\nCity " + city;
+    }    
+}
